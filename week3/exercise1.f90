bc0ba11b767e35238bfb7c1182e5cff6281d1a75
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-03-16 22:47:28 +0100

    formatter

--- week3/exercise1.f90
+++ week3/exercise1.f90
@@ -1,19 +1,18 @@
 program main
-    implicit none
-    integer, parameter :: n=10000000
-    real, dimension(n) :: y
+   implicit none
+   integer, parameter :: n = 10000000
+   real, dimension(n) :: y
 
-    call draw_exponential_rv(y)
-    WRITE (unit=1, fmt=*) y
- 
- contains
- 
-    subroutine draw_exponential_rv(x)
-        real, dimension(:), intent(inout) :: x
+   call draw_exponential_rv(y)
+   WRITE (unit=1, fmt=*) y
 
-        call random_number(x)
-        x = -log(x)
-    end subroutine
- 
- end program main
- 
+contains
+
+   subroutine draw_exponential_rv(x)
+      real, dimension(:), intent(inout) :: x
+
+      call random_number(x)
+      x = -log(x)
+   end subroutine
+
+end program main
--- week3/exercise2.f90
+++ week3/exercise2.f90
@@ -1,44 +1,43 @@
 program main
-    implicit none
-    integer, parameter :: n=10000000
-    real, parameter :: pi=3.14159265359
-    real, dimension(n) :: y1, y2
+   implicit none
+   integer, parameter :: n = 10000000
+   real, parameter :: pi = 3.14159265359
+   real, dimension(n) :: y1, y2
 
-    call draw_method_1(y1)
-    WRITE (unit=1, fmt=*) y1
-    call draw_method_2(y2)
-    WRITE (unit=2, fmt=*) y2
- 
- contains
- 
-    subroutine draw_method_1(x)
-        real, dimension(:), intent(inout) :: x
+   call draw_method_1(y1)
+   WRITE (unit=1, fmt=*) y1
+   call draw_method_2(y2)
+   WRITE (unit=2, fmt=*) y2
 
-        call random_number(x)
-        x = cos(pi * x)
-    end subroutine
+contains
 
-    subroutine draw_method_2(u)
-        real, dimension(:), intent(inout) :: u
-        real, dimension(size(u)) :: v, u2, v2
-        integer :: i
+   subroutine draw_method_1(x)
+      real, dimension(:), intent(inout) :: x
 
-        call random_number(u)
-        call random_number(v)
-        u2 = u*u
-        v2 = v*v
+      call random_number(x)
+      x = cos(pi*x)
+   end subroutine
 
-        do i=1,n
-            do while (u2(i)+v2(i) > 1)
-                call random_number(u(i))
-                call random_number(v(i))
-                u2(i) = u(i)*u(i)
-                v2(i) = v(i)*v(i)
-            enddo
-        end do
+   subroutine draw_method_2(u)
+      real, dimension(:), intent(inout) :: u
+      real, dimension(size(u)) :: v, u2, v2
+      integer :: i
 
-        u = (u2 - v2) / (u2 + v2)
-    end subroutine
- 
- end program main
- 
+      call random_number(u)
+      call random_number(v)
+      u2 = u*u
+      v2 = v*v
+
+      do i = 1, n
+         do while (u2(i) + v2(i) > 1)
+            call random_number(u(i))
+            call random_number(v(i))
+            u2(i) = u(i)*u(i)
+            v2(i) = v(i)*v(i)
+         end do
+      end do
+
+      u = (u2 - v2)/(u2 + v2)
+   end subroutine
+
+end program main
