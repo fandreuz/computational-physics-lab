c8ffed54765befa69446906fe7a4d5b8cc73422f
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-03-28 00:59:56 +0200

    minor fixes

--- week4/exercise2.f90
+++ week4/exercise2.f90
@@ -1,9 +1,9 @@
 program main
    implicit none
-   integer, parameter :: l = 1, N = 100, runs = 10000
+   integer, parameter :: l = 1, N = 1000, runs = 10
    real, parameter :: p_right = 0.5
-   real, dimension(N) :: moves, l_vec
-   real, dimension(N) :: x, avg_position, avg_sq_position
+   real :: xi2
+   real, dimension(N) :: moves, l_vec, x, avg_position, avg_sq_position
    integer :: i, run
 
    OPEN (unit=1, file="position.dat", status="replace", action="write")
@@ -21,15 +21,21 @@ program main
       x(:) = sign(l_vec, p_right - moves)
       do i = 2, n
          x(i) = x(i) + x(i - 1)
+         xi2 = x(i)*x(i)
+
+         ! sum across different particles to perform an ensemble average
          avg_position(i) = avg_position(i) + x(i)
-         avg_sq_position(i) = avg_sq_position(i) + x(i)*x(i)
+         avg_sq_position(i) = avg_sq_position(i) + xi2
       end do
-      
+
       WRITE (unit=1, fmt=*) x
    end do
 
-   WRITE (unit=2, fmt=*) avg_position / runs
-   WRITE (unit=2, fmt=*) avg_sq_position / runs
+   avg_position(:) = avg_position/runs
+   avg_sq_position(:) = avg_sq_position/runs
+   WRITE (unit=2, fmt=*) avg_position
+   WRITE (unit=2, fmt=*) avg_sq_position
+   WRITE (unit=2, fmt=*) avg_sq_position - avg_position**2
 
 contains
 
