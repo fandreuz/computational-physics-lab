cefd3e372db8a0de92e04a5b39e24711d2a172b6
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-05-28 15:44:23 +0200

    implement linear fit

--- week9/plot_msd.py
+++ week9/plot_msd.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
+from sklearn.linear_model import LinearRegression
 
 font = {"size": 15}
 matplotlib.rc("font", **font)
@@ -18,13 +19,26 @@ plt.title(r"$\langle R^2(t) \rangle$")
 
 plt.plot(data[:, 0], data[:, 1], "-", label=r"Simulation")
 plt.plot(data[:, 0], a * a * np.arange(1, len(data) + 1) * skip, "-", label="$a^2 N$")
+
+limit = (L * a / 2) ** 2
 plt.hlines(
-    (L * a / 2) ** 2,
+    limit,
     data[:, 0].min(),
     data[:, 0].max(),
     colors="r",
     linestyles="--",
     label="Th. limit",
+)
+
+under_limit_count = len(data[:, 1] < limit)
+reg = LinearRegression(fit_intercept=False).fit(
+    np.arange(under_limit_count)[:, None] * skip, data[:under_limit_count, 1][:, None]
+)
+plt.plot(
+    data[:, 0],
+    reg.coef_[0] * np.arange(1, len(data) + 1) * skip,
+    "-",
+    label="Linear fit",
 )
 
 plt.legend()
