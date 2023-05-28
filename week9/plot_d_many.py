27b218a12ab0cee471eff6923263e8e20b9d2370
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-05-28 23:16:17 +0200

    first version

---
+++ week9/plot_d_many.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+font = {"size": 15}
+matplotlib.rc("font", **font)
+
+seeds = list(range(4))
+datas = [np.loadtxt(f"fort.1_{seed}")[:, [0, 3]] for seed in seeds]
+
+plt.figure(figsize=(20, 12))
+
+plt.title("Diffusion coefficient")
+
+plt.xlabel("t")
+
+
+def compute_block_mean(data):
+    return np.mean([np.mean(block) for block in np.array_split(data[:, 1], 5)])
+
+
+print(np.mean(list(map(compute_block_mean, datas))))
+
+for idx, data in enumerate(datas):
+    block_mean = compute_block_mean(data)
+    plt.subplot(2, 2, idx + 1)
+    plt.plot(data[:, 0], data[:, 1], label=r"$D(t)$")
+    plt.hlines(
+        block_mean,
+        data[:, 0].min(),
+        data[:, 0].max(),
+        colors="r",
+        linestyles="dashed",
+        label=r"$\langle D_{}\rangle_T$",
+    )
+
+    plt.legend()
+    plt.grid()
+
+plt.show()
