04f94c88f52d4e6f970f5492a104de99da53f50e
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-05-28 21:35:35 +0200

    cleanup

--- week9/plot_d.py
+++ week9/plot_d.py
@@ -5,9 +5,6 @@ import matplotlib
 font = {"size": 15}
 matplotlib.rc("font", **font)
 
-a = 2.0e-8
-dt = 1.0e-9
-
 data = np.loadtxt("fort.1")[:, [0, 3, 4]]
 
 plt.figure(figsize=(10, 6))
@@ -23,9 +20,6 @@ plt.hlines(
     data[:, 0].max(),
     colors="g",
     label=r"$\langle D\rangle_T$",
-)
-plt.hlines(
-    a * a / (4 * dt), data[:, 0].min(), data[:, 0].max(), colors="r", label=r"$D_{th}$"
 )
 
 plt.legend()
